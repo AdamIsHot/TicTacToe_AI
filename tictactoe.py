6bf13f3621e50ceed06e57afee19c4bf628cbcc8
AdamJeHot <adam.jerhot@student.gyarab.cz>
2022-04-23 21:32:12 +0200

    funkcnejsi picovina

--- tictactoe.py
+++ tictactoe.py
@@ -5,24 +5,46 @@ pygame.init()
 
 WIDTH = 600
 HEIGHT = 600
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0,255, 0)
+
+LINE_COLOR = (131,139,139)
+PAPER_COLOR = (240,255,255)
+CIRCLE_COLOR = (0, 0, 255)
+CROSS_COLOR = (255, 0, 0)
+
 LINE_WIDTH = 15
+
 BOARD_ROWS = 3
 BOARD_COLS = 3
-LOL = 3
+
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+
+CROSS_WIDTH = 25
+SPACE = 55
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TicTacToeAI')
-screen.fill(RED)
+screen.fill(PAPER_COLOR)
 
 board = np.zeros((BOARD_ROWS, BOARD_COLS))
-print(board)
+
+def draw_figures():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+            elif board[row][col] == 2:
+                pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH)
+                
 
 def draw_lines():
-    pygame.draw.line(screen, GREEN, (0, 200), (600, 200), LINE_WIDTH)
-    pygame.draw.line(screen, GREEN, (0, 400), (600, 400), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (0, 400), (600, 400), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+
 
 def mark_square(row, col, player):
     board[row][col] = player
@@ -40,14 +62,30 @@ def is_board_full():
 
 
 
-mark_square(0, 0, 1)
-mark_square(1, 1, 2)
-print(available_square(1, 1))
-print(board)
+
 draw_lines()
+
+player = 1
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX = event.pos[0]
+            mouseY = event.pos[1]
+
+            clicked_row = int(mouseY // 200)
+            clicked_col = int(mouseX // 200)
+
+            if available_square(clicked_row, clicked_col):
+                if player == 1:
+                    mark_square(clicked_row, clicked_col, 1)
+                    player = 2
+                elif player == 2:
+                    mark_square(clicked_row, clicked_col, 2)
+                    player = 1
+                draw_figures()
+
     pygame.display.update()
