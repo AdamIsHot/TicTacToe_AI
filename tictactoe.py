a0edad136cd41ef06e11c4a368cf81600298ad0c
AdamJeHot <adam.jerhot@student.gyarab.cz>
2022-05-05 19:55:23 +0200

    opraveno zpet na 3x3, AI zjistuje jake pozice jsou zabrane

--- tictactoe.py
+++ tictactoe.py
@@ -1,28 +1,27 @@
 import pygame, sys
 import numpy as np
+import random
 
 pygame.init()
 
-screen = pygame.display.set_mode()
-
-WIDTH, HEIGHT = screen.get_size()
+WIDTH = 600
+HEIGHT = 600
 
 LINE_COLOR = (131,139,139)
 PAPER_COLOR = (240,255,255)
 CIRCLE_COLOR = (0, 0, 255)
 CROSS_COLOR = (255, 0, 0)
 
-LINE_WIDTH = 2
+LINE_WIDTH = 15
 
-BOARD_ROWS = 1000
-BOARD_COLS = 1000
+BOARD_ROWS = 3
+BOARD_COLS = 3
 
-CIRCLE_RADIUS = 6
-CIRCLE_WIDTH = 1
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
 
-CROSS_WIDTH = 2
-SPACE = 5
-
+CROSS_WIDTH = 25
+SPACE = 55
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -31,27 +30,24 @@ screen.fill(PAPER_COLOR)
 
 board = np.zeros((BOARD_ROWS, BOARD_COLS))
 
+
 def draw_figures():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             if board[row][col] == 1:
-                pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 20 + 10), int(row * 20 + 10)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+                pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
             elif board[row][col] == 2:
-                pygame.draw.line(screen, CROSS_COLOR, (col * 20 + SPACE, row * 20 + 20 - SPACE), (col * 20 + 20 - SPACE, row * 20 + SPACE), CROSS_WIDTH)
-                pygame.draw.line(screen, CROSS_COLOR, (col * 20 + SPACE, row * 20 + SPACE), (col * 20 + 20 - SPACE, row * 20 + 20 - SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH)
                 
 
 def draw_lines():
-    divider_for_squares = 20
-    WIDTH_SQUARES = int(WIDTH / divider_for_squares) # how many squares are on width side of paper
-    HEIGHT_SQUARES = int(HEIGHT / divider_for_squares) # how many squares are on height side of paper
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (0, 400), (600, 400), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
 
-    for x in range(WIDTH_SQUARES):
-        pygame.draw.line(screen, LINE_COLOR, (x * divider_for_squares, 0), (x * divider_for_squares, HEIGHT), LINE_WIDTH)
-    
-    for y in range(HEIGHT_SQUARES):
-        pygame.draw.line(screen, LINE_COLOR, (0, y * divider_for_squares), (WIDTH, y * divider_for_squares), LINE_WIDTH)
-    
+
 def mark_square(row, col, player):
     board[row][col] = player
 
@@ -127,14 +123,40 @@ def draw_desc_diagonal(player):
 
 
 def restart():
-    screen.fill(PAPER_COLOR)
+    screen.fill(BG_COLOR)
     draw_lines()
     player = 1
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             board[row][col] = 0
 
+def AI_move(moves_row, moves_col):
+    ai_row_move = 0
+    ai_col_move = 0
 
+    #define new position for rows
+    row_done = False
+    while row_done == False:
+        new_row = random.randint(0, 2)
+        if new_row not in moves_row:
+            ai_row_move = new_row
+            row_done = True
+
+    #define new position for cols
+    col_done = False
+    while col_done == False:
+        new_col = random.randint(0, 2)
+        if new_col not in moves_col:
+            ai_col_move = new_col
+            col_done = True
+    
+    return ai_row_move, ai_col_move
+    
+    
+
+    
+    
+    
 
 
 
@@ -145,6 +167,9 @@ draw_lines()
 
 player = 1
 game_over = False
+moves_row = []
+moves_col = []
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -154,21 +179,30 @@ while True:
             mouseX = event.pos[0]
             mouseY = event.pos[1]
 
-            clicked_row = int(mouseY // 20)
-            clicked_col = int(mouseX // 20)
+            clicked_row = int(mouseY // 200)
+            clicked_col = int(mouseX // 200)
 
             if available_square(clicked_row, clicked_col):
-                if player == 1:
-                    mark_square(clicked_row, clicked_col, 1)
-                    if check_win(player):
-                        game_over = True
-                    player = 2
-                elif player == 2:
-                    mark_square(clicked_row, clicked_col, 2)
-                    if check_win(player):
-                        game_over = True
-                    player = 1
+                mark_square(clicked_row, clicked_col, 1)
+                clicked_row_array = [clicked_row]
+                clicked_col_array = [clicked_col]
+                moves_row = moves_row + clicked_row_array
+                moves_col = moves_col + clicked_col_array
+
+                if check_win(player):
+                    game_over = True
+                
                 draw_figures()
+                
+                ai_row_move, ai_col_move = AI_move(moves_row, moves_col)
+                print(ai_row_move, ai_col_move)
+
+                
+                
+
+
+                
+        
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 restart()
