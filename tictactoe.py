959dbdf712fe5d77f63daf932f3e15e2d899a4b4
AdamJeHot <adam.jerhot@student.gyarab.cz>
2022-05-25 02:21:48 +0200

    uz nwm nachcal jsem se...

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,6 @@ import numpy as np
 import random
 import time
 
-
 WIDTH = 600
 HEIGHT = 600
 
@@ -36,14 +35,9 @@ def draw_cross():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             if board[row][col] == 1:
-                #pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 200 + 100), int(row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
                 pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH)
                 pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH)
-                
-            #elif board[row][col] == 2:
-                #pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH)
-                #pygame.draw.line(screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH)
-                
+
 def draw_circle():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
@@ -84,11 +78,11 @@ def check_win(player):
             return False
         
     if board[2][0] == player and board[1][1] == player and board[row][2] == player:
-        draw_asc_diagonal(player)
+        draw_asc_diagonal()
         return True
 
     if board[0][0] == player and board[1][1] == player and board[2][2] == player:
-        draw_desc_diagonal(player)
+        draw_desc_diagonal()
         return True
 
     return False
@@ -138,12 +132,14 @@ moves_col = []
 
 
 
+
 while True:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+        
+        #hra zahajuje hru hrace
         if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
             mouseX = event.pos[0]
             mouseY = event.pos[1]
@@ -151,7 +147,7 @@ while True:
             clicked_row = int(mouseY // 200)
             clicked_col = int(mouseX // 200)
 
-            #pro pohyb hrace nejdrive hra zjistuje zda je pole volne
+
             if available_square(clicked_row, clicked_col):
                 mark_square(clicked_row, clicked_col, 1)
                 clicked_row_array = [clicked_row]
@@ -163,8 +159,8 @@ while True:
 
                 if check_win(1):
                     game_over = True
+                    another_game()
                 pygame.display.update()
-
 
                 #nyni hra zahajuje hru AI
                 time.sleep(0.2)
@@ -179,6 +175,7 @@ while True:
 
                 if check_win(2):
                     game_over = True
+                    another_game()
                 pygame.display.update()
                 
                 
@@ -188,3 +185,4 @@ while True:
                     restart()
 
         pygame.display.update()
+
